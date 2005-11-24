324db16aee838f2aff27c270848f75ee11a6466d
oliver <oliver>
2005-11-24 04:13:09 +0000

    20051123: data validation added

---
+++ tcb_ploan/prog/error.h
@@ -0,0 +1,43 @@
+/****************************************************************************
+** Licensed Materials - Property of DAC
+**
+** (C) COPYRIGHT Decision Analytics Consulting 2005
+** All Rights Reserved.
+**
+*****************************************************************************
+** SOURCE FILE NAME: error.h
+**
+**
+****************************************************************************/
+#ifndef Error_H
+#define Error_H
+
+#define DataErrorMsg(x)  tcbMsg[(x)].msg
+#define DataErrorCode(x) tcbMsg[(x)].code
+ 
+typedef struct _invalid_msg {
+	int code;
+	char msg[128];
+} Invalid_Msg, *Invalid_Msg_Ptr;
+
+enum ErrorCodes {
+	DATA_ERR_1001,
+	DATA_ERR_1002,
+	DATA_ERR_1003,
+	DATA_ERR_1004,
+	DATA_ERR_1005,
+	DATA_ERR_1006,
+	DATA_ERR_1007,
+	DATA_ERR_1008,
+	DATA_ERR_1009,
+	DATA_ERR_1010,
+	DATA_ERR_1011,
+	DATA_ERR_1012,
+	DATA_ERR_1013,
+	DATA_ERR_1014,
+	EXIST_ERR_2001,
+	EXIST_ERR_2002,
+	EXIST_ERR_2003
+};	
+
+#endif  //Error_H
--- tcb_ploan/prog/loan_app.h
+++ tcb_ploan/prog/loan_app.h
@@ -22,24 +22,29 @@
 class LoanApp {
   private:
     /* application information */
-    char case_sn[20];                 // 案件編號
-    char aID[12];                     // 申請人身分證號
-    char gID[12];                     // 保證人身分證號
-    int product_type;               // 產品代號(1 為國民信貸; 2 為卡好借)
-    int gender;                     // 0為女性; 1為男性
-    char zip[4];                     // 三位郵遞區號
-    int edu;                        // 教育程度 ，1 為研究所(含)以上 ; 2 為大學; 3 為專科;
-                                    // 4  為高中(職); 5 為國中以下; 6 為其他
-    int marriage_status;            // 婚姻狀況,1 為已婚; 2 為未婚; 3 為離婚; 4 為其他
-    double cof;                     // 資金成本
-    double roe;                     // 股東權益報酬
-    int info_processing_cost;       // 每戶每月分攤台新銀行資訊室費用 (新台幣元)
-    int operation_cost;             // 每戶每月分攤台新銀行分行作業及客服費用 (新台幣元)
-    int hr_cost;                    // 每戶每月人事成本費用 (新台幣元)
-    double commission;              // 佣金 (新台幣元)
-    int other_initial_cost1;       // 其他開辦成本一
-    int other_initial_cost2;       // 其他開辦成本二
-    int other_initial_cost3;       // 其他開辦成本三
+    char  case_sn[20];			//案件編號
+    char  aID[12];			//申請人身分證號
+    char  gID[12];			//保證人身分證號
+    int   Loan_purpose;			//資金用途
+    int   Education;			//教育程度
+    char  Birthday[8];			//申請人生日
+    int   Marriage_status;		//申請人婚姻狀態
+    int   Annual_Income;		//申請人年所得收入
+    int   Home_ownership;		//申請人現住房屋狀況
+    int   Year_at_current_address;	//申請人現址已居住年數
+    int   Year_at_current_job;		//申請人現職服務年資
+    int   Car_year;			//申請人車籍資料(年份)
+    int   Product_type_code;		//產品種類碼
+    int   App_amt;			//申貸金額
+    int   Term_1;			//期數(第一期)
+    int   Term_2;			//期數(第二期)
+    int   Term_3;			//期數(第三期)
+    float APR_1;			//利率(第一期)
+    float APR_2;			//利率(第二期)
+    float APR_3;			//利率(第三期
+    int   Grace_period;			//寬限期
+    int   Fee_1;			//開辦費(固定金額部分)
+    float Fee_2;			//開辦費(百分比部分)
 
     int record_count;               // number of app_info records read
     int trial_count;                // number of Loan records read
@@ -70,5 +75,6 @@ class LoanApp {
     char * Guanrantor_id();
 };
 
+int validate_date(char *date);  // format of date is yyymmdd
 
 #endif  //LoanApp_H
