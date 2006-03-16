29f9c075717712755b40b2689f5d3b659816794f
oliver <oliver>
2006-03-16 03:36:25 +0000

    20060316: test driver to test dac_pl_fnl()

---
+++ tcb_ploan/prog/mainfnl.cpp
@@ -0,0 +1,69 @@
+/****************************************************************************
+** Licensed Materials - Property of DAC
+**
+** (C) COPYRIGHT Decision Analytics Consulting 2005
+** All Rights Reserved.
+**
+*****************************************************************************
+**
+** SOURCE FILE NAME: main.C 
+**    
+** Description: How to modify a database
+**
+**         This is a tutorial sample showing basic database
+**         modification. For a more comprehensive sample, see dbuse.sqC.
+**           
+****************************************************************************/
+#include <string.h>
+#include <sqlenv.h>
+#include <sqlutil.h>
+#include "utilemb.h"
+#if ((__cplusplus >= 199711L) && !defined DB2HP) || \
+    (DB2LINUX && (__LP64__ || (__GNUC__ >= 3)) )
+   #include <iostream>
+   #include <fstream>
+   using namespace std; 
+#else
+   #include <iostream.h>
+   #include <fstream.h>
+#endif
+#include "dac_pl_cal.h"
+
+
+int main(int argc, char *argv[])
+{
+  int rc = 0;
+  char alias[32], uid[32], upw[32], case_sn[16], msg[1024];
+  ifstream infile("case_list");
+//  DbEmb db;
+//  struct sqlca sqlca;
+  int index = 0;
+/*
+  EXEC SQL BEGIN DECLARE SECTION;
+    char Case_Sn[20];
+  EXEC SQL END DECLARE SECTION;
+*/
+  // check the command line arguments
+
+  strcpy (alias, "escore");
+  strcpy (uid, "escorap1");
+  strcpy (upw, "escorap1");
+  strcpy (case_sn, "000000002950");
+/*
+  strcpy (alias, "dacdb");
+  strcpy (uid, "ejcicap1");
+  strcpy (upw, "ejcicap1");
+*/
+//  rc = dac_pl_cal(case_sn, alias, uid, upw, msg);
+//  if (rc != 0) cout << CurrDateTime() << ": " << msg << endl; 
+
+  while (!infile.eof()) { 
+     infile.getline(case_sn, 16);
+     index++;
+     cout << CurrDateTime() << ": " << index << "- " << case_sn << endl; 
+     rc = dac_pl_fnl(case_sn, alias, uid, upw, msg);
+     if (rc != 0) cout << CurrDateTime() << ": " << msg << endl; 
+  }
+
+  return 0;
+} //main
