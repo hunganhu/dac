3b78618c7d1fe926d149a6ba1c8e54e8d92ada5a
oliver <oliver>
2007-10-26 02:37:22 +0000

    output applicant's pb, guarantor's pb and adjusted pb for mortgage.

--- tcb_ploan/prog/risk_model.h
+++ tcb_ploan/prog/risk_model.h
@@ -26,12 +26,12 @@ class RiskModel
 
   public:
     int PrepareJcicTables(char *case_sn, char *idn);
-    int Prescreen(char *case_sn, char *idn);
+    int Prescreen(char *case_sn, char *idn, int product_type_code);
     int GenerateScreenVars(char *case_sn, char *idn, char *birthday, char *sysdate);
     int GeneratePdacoScore(char *case_sn, char *idn);
     int SaveScore(char *case_sn, char *idn);
     int CleanTables(char *case_sn, char *idn);
-    int Calculate_PB(char *case_sn, char *idn, char *birthday, char *sysdate);
+    int Calculate_PB(char *case_sn, char *idn, char *birthday, char *sysdate, int product_type_code);
     int get_pb(char *case_sn, char *idn, float *pb);
     int get_prescreen_status (char *case_sn, char *idn, int *code, char *msg, int *unsecured_bal);
 };
